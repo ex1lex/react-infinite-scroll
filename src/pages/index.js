e7be21cf11699b7f6d66a9e4229a87097dd8df7b
Alexey Obryadin <obryadin.alexey@gmail.com>
2023-11-16 13:33:24 +0300

    create pages and routes

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+// App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './main';
+import PostDetail from './post-detail';
+
+const Router = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="" element={<Main />} />
+				<Route path=":postId" element={<PostDetail />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default Router;
